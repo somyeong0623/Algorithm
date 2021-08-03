fc16a89bfb28d5c0b34f74e094271ea39aea2dc6
somyeong0623 <thaud8487@naver.com>
2021-08-03 22:00:36 +0900

    Create 10430.cpp
    백준 10430: 나머지

---
+++ 10430.cpp
@@ -0,0 +1,15 @@
+//10430
+//³ª¸ÓÁö
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << (a + b) % c << "\n";
+	cout << ((a % c) + (b % c)) % c << "\n";
+	cout << (a * b) % c << "\n";
+	cout << ((a % c) * (b % c)) % c << "\n";
+}
