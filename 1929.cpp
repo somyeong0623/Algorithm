3cf277359654efbc2e4f0d6558f9576e050972a6
somyeong0623 <thaud8487@naver.com>
2021-08-06 17:41:13 +0900

    Create 1929.cpp
    백준 1929: 소수 구하기

---
+++ 1929.cpp
@@ -0,0 +1,28 @@
+//1929. 소수 구하기
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool func(int x) {
+	if (x == 1)
+		return 0;
+	for (int i = 2; i <=sqrt(x); i++) {
+		if (x % i == 0)
+			return 0;
+	}
+	return 1;
+
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int x, y;
+	cin >> x >> y;
+	for (int i = x; i <= y; i++) {
+		if (func(i))
+			cout << i << "\n";
+	}
+
+}
