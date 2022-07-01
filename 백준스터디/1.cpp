af091546523de569d4e753b90226d0e203f52592
somyeong <thaud8487@naver.com>
2022-07-01 18:24:45 +0900

    [백준] 2178. 미로탐색

--- 백준스터디/1.cpp
+++ 백준스터디/1.cpp
@@ -1,49 +1,62 @@
+//2178번: 미로탐색
+
+/*
+
+2<=N,M,M=<=100
+
+*/
+
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <queue>
-
-#define INF 987654321
-
 using namespace std;
+int n,m;
+char arr[100][100];
+int answer[100][100];
+bool visit[100][100];
+int dx[]={1,0,-1,0};
+int dy[]={0,1,0,-1};
 
-vector<pair<int,int>> arr[5001];
-bool ch[5001];
+queue<pair<int,int>> q;
 
-int main() {
-	int N, Q;
-	cin >> N >> Q;
-	for (int i = 0; i < N-1; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		arr[a].push_back({ b,c });
-		arr[b].push_back({ a,c });
-	}
-	for (int i = 0; i < Q; i++) {
-		int k, nth;
-		cin >> k >> nth;
-		memset(ch, false, sizeof(ch));
-		ch[nth] = true;
-		int cnt = 0;
-		queue<int> q;
-		q.push(nth);
-		while (!q.empty()) {
-			int now = q.front();
-			q.pop();
-			for (int i = 0; i < arr[now].size(); i++) {
-				int next = arr[now][i].first;
-				int len = arr[now][i].second;
-				if (ch[next]) continue; 
-				if (len >= k) {
-					cnt++;
-					ch[next] = true;
-					q.push(next);
-				}
-			}
-		}
-		cout << cnt << "\n";
-	}
+void bfs(int x,int y){  
+    answer[x][y]=1;
+    visit[x][y]=true;
+    q.push(make_pair(x,y));
 
-	return 0;
+    while(!q.empty()){
+        int cx=q.front().first;
+        int cy=q.front().second;
+        // cout<<"cx : "<<cx<<", cy: "<<cy<<"\n";
+        q.pop();
+
+        for(int i=0; i<4; i++){
+            int nx=cx+dx[i];
+            int ny=cy+dy[i];
+
+            if(nx<0 || ny<0 || nx>=n || ny>=m)
+                continue;
+            
+            if(arr[nx][ny]=='1' && visit[nx][ny]==false){
+                answer[nx][ny]=answer[cx][cy]+1;
+                q.push(make_pair(nx,ny));
+                visit[nx][ny]=true;
+            }
+        }
+
+    }
+
+}
+
+
+int main(){
+    cin>>n>>m;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+    bfs(0,0);
+    cout<<answer[n-1][m-1]<<"\n";
+
+
 }
