8d1c99b4be99b168a12828aa3ec2e4db5b88b6ac
이소명 <thaud8487@naver.com>
2022-07-26 21:22:12 +0900

    [백준] 18116. 로봇 조립

--- 분리집합/18116.로봇조립.cpp
+++ 분리집합/18116.로봇조립.cpp
@@ -5,11 +5,12 @@
 */
 #include <iostream>
 #include <vector>
+#define N 1000005
 using namespace std;
 
 int n;
-vector<int> parent(1000001);
-vector<int> cnt(1000001,1); //부모의 갯수 벡터, 처음엔 1로 초기화
+vector<int> parent(N);
+vector<int> cnt(N,1); //부모의 갯수 벡터, 처음엔 1로 초기화
 
 int getParent(int x){
     if(x==parent[x])
@@ -22,18 +23,21 @@ void unionParent(int x, int y){
     x=getParent(x);
     y=getParent(y);
 
+    if(x==y) //이미 같은 부품이면 cnt 증가시키면 안되니까 함수 끝내기
+        return;
+
     if(x>y){
         parent[x]=y;
-        cnt[y]++; //y가 부모인 부품 갯수 증가
+        cnt[y]+=cnt[x]; //y가 부모인 부품 갯수 증가
     }
     else{
         parent[y]=x;
-        cnt[x]++; //x가 부모인 부품 갯수 증가
+        cnt[x]+=cnt[y];
     }
 }
 
 int getCnt(int x){
-    int p = parent[x];
+    int p = getParent(x); //여기때문에 계속 틀림!! 그냥 부모가 아니라 루트 부모를 찾아야함.
     
     return cnt[p];
 }
@@ -43,7 +47,7 @@ int main(){
     cin.tie(NULL);
     cin>>n;
 
-    for(int i=0; i<1000001; i++){
+    for(int i=0; i<=N; i++){
         parent[i]=i;
     }
     
@@ -61,6 +65,11 @@ int main(){
             cout<<getCnt(z)<<"\n";
         }
     }
+
+    // cout<<"\n";
+    // for(int i=1; i<=n; i++){
+    //     cout<<cnt[i]<<" ";
+    // }
 
 
 }
